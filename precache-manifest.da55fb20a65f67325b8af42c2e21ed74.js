bc987c17ab4612a43125854d32cc33dff71ab61b
Devitar <devin.curtis1210@gmail.com>
2022-05-08 00:19:47 -0600

    Updates

---
+++ precache-manifest.da55fb20a65f67325b8af42c2e21ed74.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5d7c21111e011fc4c959aae09f6df342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "2f7f9d9a7d0609538078",
+    "url": "/static/css/main.27de9e14.chunk.css"
+  },
+  {
+    "revision": "2f7f9d9a7d0609538078",
+    "url": "/static/js/main.2f7f9d9a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  },
+  {
+    "revision": "c0c3d2f9775eb8d877a38c12de3c4c0c",
+    "url": "/static/media/game_background.0bcec96b.png"
+  },
+  {
+    "revision": "ba4089fcf9c9c1ce4d976e7f374564cc",
+    "url": "/static/media/game_background1.c3947b62.png"
+  },
+  {
+    "revision": "7f16561103803acb56ea521e584a72ff",
+    "url": "/static/media/game_background2.58094722.png"
+  },
+  {
+    "revision": "326f907e7d63bed9bf8828dd5e26e864",
+    "url": "/static/media/game_background3.46716c72.png"
+  },
+  {
+    "revision": "7f9b39ddaa7350eedeec93311bc2ac9c",
+    "url": "/static/media/game_background4.9b742495.png"
+  },
+  {
+    "revision": "960a4dc10f2c74a6eaa0cfca4caabbb0",
+    "url": "/static/media/game_background5.845f02ba.png"
+  },
+  {
+    "revision": "1db1a5737f5f793cf1eccdee75fcbbc6",
+    "url": "/static/media/game_background6.96e0b054.png"
+  },
+  {
+    "revision": "a7cfc23508fb88390dcc3495af061c7d",
+    "url": "/static/media/paper_texture.7fac5f2d.jpg"
+  }
+]);
